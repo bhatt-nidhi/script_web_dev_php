069231733944c95cbbe881feac05af4448f0d6f1
bhatt-nidhi <143687396+bhatt-nidhi@users.noreply.github.com>
2023-10-04 13:48:15 +0530

    Add files via upload

---
+++ script_14.php
@@ -0,0 +1,11 @@
+<?php
+$a="
+<ul>
+
+	
+	<li><font color='orange'> list </font> <font color='red'> 1 </font></li> <br>
+	<li><font color='orange'> list </font> <font color='red'> 2 </font></li> <br>
+	<li><font color='orange'> list </font> <font color='red'> 3 </font></li> <br>
+	</ul>"
+echo $a;
+?>
